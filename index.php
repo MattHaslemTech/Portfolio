23f7abbf49b38919b46b538777a6a1947c526dfa
mhaslem <mhasl002@odu.edu>
2022-03-08 09:38:43 -0500

    hero edits

--- index.php
+++ index.php
@@ -15,7 +15,28 @@
         <div class="background-wrap">
           <div class="background-colors dark"> </div>
           <div class="background-colors light"></div>
-          <canvas id="bg-canvas"></canvas>
+          <!--canvas#bg-canvas-->
+          <div class="circles-wrap">
+            <div class="inner"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 225.74 225.55">
+  <defs>
+    <style>
+      .center1-cls-1 {
+        fill: none;
+        stroke: #1ab1a4;
+        stroke-miterlimit: 10;
+        stroke-width: 4px;
+        opacity: 0.53;
+      }
+    </style>
+  </defs>
+  <g id="Layer_2" data-name="Layer 2">
+    <g id="Layer_1-2" data-name="Layer 1"><path class="center1-cls-1" d="M218.68,145.61A110.54,110.54,0,0,0,89.82,4.5"/><path class="center1-cls-1" d="M7.07,79.93A110.53,110.53,0,0,0,112.44,223.55a111.51,111.51,0,0,0,23.49-2.5"/></g>
+  </g>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 199.08 224.74"><defs><style>.center2-cls-1{fill:none;stroke:#1ab1a4;stroke-miterlimit:10;stroke-width:4px;opacity:0.53;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="center2-cls-1" d="M99.27,2A110.64,110.64,0,0,0,1.78,62"/><path class="center2-cls-1" d="M99.81,222.74a110.58,110.58,0,0,0,97.49-60"/></g></g></svg>
+
+            </div>
+          </div>
         </div>
         <div class="content-wrap">
           <h1>Matt Haslem</h1>

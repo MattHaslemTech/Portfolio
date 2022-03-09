01c404810772994a01182316e98a9efae61002fd
mhaslem <mhasl002@odu.edu>
2022-03-08 22:29:50 -0500

    Mobile design changes. Added full-width sections.

--- index.php
+++ index.php
@@ -10,8 +10,127 @@
     <title>Matt Haslem - Web Developer</title>
   </head>
   <body data-theme="light">
+    <div id="hero" class="section larger-than-tablet-hide">
+      <div class="background-wrap">
+        <div class="background-colors dark"> </div>
+        <div class="background-colors light"></div>
+        <!--canvas#bg-canvas-->
+        <div class="circles-wrap">
+          <div class="inner"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 225.74 225.55">
+  <defs>
+    <style>
+      .center1-cls-1 {
+        fill: none;
+        stroke-miterlimit: 10;
+        stroke-width: 2px;
+        opacity: 0.53;
+      }
+    </style>
+  </defs>
+  <g id="Layer_2" data-name="Layer 2">
+    <g id="Layer_1-2" data-name="Layer 1"><path class="center1-cls-1" d="M218.68,145.61A110.54,110.54,0,0,0,89.82,4.5"/><path class="center1-cls-1" d="M7.07,79.93A110.53,110.53,0,0,0,112.44,223.55a111.51,111.51,0,0,0,23.49-2.5"/></g>
+  </g>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 157.11 222.05">
+  <defs>
+    <style>
+      .center2-cls1 {
+        fill: none;
+        
+        stroke-miterlimit: 10;
+        stroke-width: 2px;
+        isolation: isolate;
+        opacity: 0.53;
+      }
+    </style>
+  </defs>
+  <g id="Layer_2" data-name="Layer 2">
+    <g id="Layer_1-2" data-name="Layer 1">
+      <g id="Layer_2-2" data-name="Layer 2">
+        <g id="Layer_1-2-2" data-name="Layer 1-2"><path class="center2-cls1" d="M156.41,32.79A110.63,110.63,0,0,0,45,6.28"/><path class="center2-cls1" d="M.7,189.26a110.59,110.59,0,0,0,111.36,26.5"/></g>
+      </g>
+    </g>
+  </g>
+</svg>
+
+          </div>
+          <div class="second"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 225.74 225.55">
+  <defs>
+    <style>
+      .second1-cls-1 {
+        fill: none;
+        stroke-miterlimit: 10;
+        stroke-width: 4px;
+        opacity: 0.53;
+      }
+    </style>
+  </defs>
+  <g id="Layer_2" data-name="Layer 2">
+    <g id="Layer_1-2" data-name="Layer 1"><path class="second1-cls-1" d="M218.68,145.61A110.54,110.54,0,0,0,89.82,4.5"/><path class="second1-cls-1" d="M7.07,79.93A110.53,110.53,0,0,0,112.44,223.55a111.51,111.51,0,0,0,23.49-2.5"/></g>
+  </g>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 199.08 224.74">
+  <defs>
+    <style>
+      .second2-cls-1 {
+        fill: none;
+        stroke-miterlimit: 10;
+        stroke-width: 4px;
+        opacity: 0.53;
+      }
+    </style>
+  </defs>
+  <g id="Layer_2" data-name="Layer 2">
+    <g id="Layer_1-2" data-name="Layer 1"><path class="second2-cls-1" d="M99.27,2A110.64,110.64,0,0,0,1.78,62"/><path class="second2-cls-1" d="M99.81,222.74a110.58,110.58,0,0,0,97.49-60"/></g>
+  </g>
+</svg>
+
+          </div>
+          <div class="outer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 225.74 225.55">
+  <defs>
+    <style>
+      .outer1-cls-1 {
+        fill: none;
+        stroke-miterlimit: 10;
+        stroke-width: 3px;
+        opacity: 0.53;
+      }
+    </style>
+  </defs>
+  <g id="Layer_2" data-name="Layer 2">
+    <g id="Layer_1-2" data-name="Layer 1"><path class="outer1-cls-1" d="M218.68,145.61A110.54,110.54,0,0,0,89.82,4.5"/><path class="outer1-cls-1" d="M7.07,79.93A110.53,110.53,0,0,0,112.44,223.55a111.51,111.51,0,0,0,23.49-2.5"/></g>
+  </g>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 199.08 224.74">
+  <defs>
+    <style>
+      .outer2-cls-1 {
+        fill: none;
+        stroke-miterlimit: 10;
+        stroke-width: 4px;
+        opacity: 0.53;
+      }
+    </style>
+  </defs>
+  <g id="Layer_2" data-name="Layer 2">
+    <g id="Layer_1-2" data-name="Layer 1"><path class="outer2-cls-1" d="M99.27,2A110.64,110.64,0,0,0,1.78,62"/><path class="outer2-cls-1" d="M99.81,222.74a110.58,110.58,0,0,0,97.49-60"/></g>
+  </g>
+</svg>
+
+          </div>
+        </div>
+      </div>
+      <div class="content-wrap ignore-width">
+        <h1>Matt Haslem</h1>
+        <div class="caption-wrap">
+          <div class="hr"></div>
+          <div class="text">Web Developer</div>
+          <div class="hr"></div>
+        </div>
+      </div>
+    </div>
     <div id="all-content-wrap">
-      <div id="hero" class="section">    
+      <div id="hero" class="section tablet-hide"> 
         <div class="background-wrap">
           <div class="background-colors dark"> </div>
           <div class="background-colors light"></div>
